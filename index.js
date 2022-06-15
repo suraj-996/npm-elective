4e8fa71b638eba5527c80b24bb6f73d583a315c3
Suraj Kumar <surajkumarjupiter@gmail.com>
2022-06-15 23:36:06 +0530

    elective-npm

---
+++ index.js
@@ -0,0 +1,15 @@
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
